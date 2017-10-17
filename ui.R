a62aee864829bd38517d3257c4a652c6a58741d5
Lauren Hoffman <hoffmanl18@mail.wlu.edu>
2017-10-17 13:03:28 -0400

    Updated ui.R page to health outcomes with example data

--- ui.R
+++ ui.R
@@ -7,25 +7,26 @@ Sample_R_Data <- read_xlsx("Sample R Data.xlsx")
 # Sidebar with a slider input
 fluidPage(
   
-  titlePanel('Recent Grads Info!' , 
+  titlePanel('Health Outcomes by County' , 
              
              sidebarLayout(
                #panel with widgets
                sidebarPanel(
-                 selectInput(inputId = "Major Categor", 
-                             label = 'Select a major category' , 
-                             choices = unique(recent_grads$Major_category) , 
-                             selectInput(inputId = 'value' ,
-                                         label = 'What to plot' ,
-                                         choices = colnames(recent_grads) [5:ncol(recent_grads)])
-                             #gives choices to plot y axis - columns 5-nth value on data set
-                 )
+                 selectInput(inputId = "State", 
+                             label = 'Select a state' , 
+                             choices = unique(Sample_R_Data$State)
+                            
+                             
+                             # selectInput(inputId = 'value' ,
+                             #label = 'What to plot' ,
+                            # choices = colnames(recent_grads) [5:ncol(recent_grads)])
+               
                )
              )
   )
-)
+))
 
 #Panel plot
 mainPanel(
-  plotOutput('employed_plot')
+  plotOutput('health_plot')
 )
