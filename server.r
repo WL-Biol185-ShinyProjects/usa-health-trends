320b7a4c5e6b60b60e60494f9e81e076b9fcca43
Tyra Barrett <barrettt18@mail.wlu.edu>
2017-10-17 13:17:07 -0400

    debugged code based off in class example

--- server.r
+++ server.r
@@ -1,24 +1,23 @@
-mainPanel(
-  plotOutput('employed_plot')
-)
 
 #IN SERVER.R
 library(shiny)
 library(tidyverse)
 library(ggplot2)
 
+Sample_R_Data <- read_xlsx("Sample R Data.xlsx")
+
 #define server logic to draw historgram 
 funciton(input, output) {
   
-  output$employed_plot <- renderPlot ({
+  output$health_plot <- renderPlot ({
     
-    recent_grads %>%
-      filter(Major_category == input$major_category) %>%
-      ggplot(aes(Major, Employed)) + geom_bar(stat = "identity")
+    Sample_R_Data %>%
+      filter(State == input$State) %>%
+      ggplot(aes_string("County", input$value)) + geom_bar(stat = "identity")
     #this line specifies to plot what the user selects as the category in the dropdown
     
   })}
 
   
-}
+
 
--- ui.R
+++ ui.R
@@ -7,24 +7,25 @@ Sample_R_Data <- read_xlsx("Sample R Data.xlsx")
 # Sidebar with a slider input
 fluidPage(
   
-  titlePanel('Health Outcomes by County' , 
+  titlePanel('Health Outcomes by County') , 
              
              sidebarLayout(
                #panel with widgets
                sidebarPanel(
-                 selectInput(inputId = "State", 
+                 selectInput(inputId = 'State', 
                              label = 'Select a state' , 
                              choices = unique(Sample_R_Data$State)
+                             ),
                             
                              
-                             # selectInput(inputId = 'value' ,
-                             #label = 'What to plot' ,
-                            # choices = colnames(recent_grads) [5:ncol(recent_grads)])
+                             selectInput(inputId = 'value' ,
+                             label = 'select value' ,
+                            choices = colnames(Sample_R_Data) [5:ncol(Sample_R_Data)])
                
                )
              )
-  )
-))
+  ),
+
 
 #Panel plot
 mainPanel(
