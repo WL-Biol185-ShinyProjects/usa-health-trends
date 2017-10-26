d95182468fe66d45f920d6c3507f56e4b07a032d
Hannah Palmatary <palmataryh18@mail.wlu.edu>
2017-10-26 13:02:52 -0400

    First attempt to get plots with 3 variables. Changed code to incorporate full and updated data set

--- server.r
+++ server.r
@@ -4,8 +4,13 @@ library(tidyverse)
 library(ggplot2)
 library(readxl)
 
-Sample_R_Data <- read_xlsx("Sample R Data.xlsx")
-colnames(Sample_R_Data) <-c("FIPS", "State", "County", "HO_Rank", "HO_Quartile", "HF_Rank", "HF_Quartile")
+USA_health_trends_ranked_measure_data <- read_xlsx("USA health trends ranked measure data.xlsx", 
+                                                   header     = TRUE,
+                                                   na.strings = "***"
+)
+
+#Sample_R_Data <- read_xlsx("Sample R Data.xlsx")
+#colnames(Sample_R_Data) <-c("FIPS", "State", "County", "HO_Rank", "HO_Quartile", "HF_Rank", "HF_Quartile")
 
 
 #define server logic to draw historgram 
@@ -13,15 +18,20 @@ function(input, output) {
   
   output$health_plot <- renderPlot ({
 
-    Sample_R_Data[Sample_R_Data$State == input$State,] %>%
-      ggplot(aes_string("County", input$value)) + geom_bar(stat = "identity") + 
-                        theme(axis.text.x = element_text(angle = 60, hjust = 1))
+    #Sample_R_Data[Sample_R_Data$State == input$State,] %>%
+     # ggplot(aes_string("County", input$value)) + geom_bar(stat = "identity") + 
+      #                  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+    
+    USA_health_trends_ranked_measure_data <- read.table("USA health trends ranked measure data.xlsx", 
+                                                        header     = TRUE,
+                                                        na.strings = "***")
+    USA_health_trends_ranked_measure_data[USA_health_trends_ranked_measure_data$State== input$State,] %>%
+      ggplot(aes_string("County", input$value1)) + geom_bar(stat = "identity", color = input$value2) + 
+      theme(axis.text.x = element_text(angle = 60, hjust = 1))
     
   })}
 
-USA_health_trends_ranked_measure_data <- read.table("USA health trends ranked measure data.xlsx", 
-                        header     = TRUE,
-                        na.strings = "***"
-)
+
+
 
 
--- ui.R
+++ ui.R
@@ -2,8 +2,12 @@ library(shiny)
 library(readxl)
 
 #load file into r table 
-Sample_R_Data <- read_xlsx("Sample R Data.xlsx")
-colnames(Sample_R_Data) <-c("FIPS", "State", "County", "HO_Rank", "HO_Quartile", "HF_Rank", "HF_Quartile")
+  #Sample_R_Data <- read_xlsx("Sample R Data.xlsx")
+  #colnames(Sample_R_Data) <-c("FIPS", "State", "County", "HO_Rank", "HO_Quartile", "HF_Rank", "HF_Quartile")
+
+USA_health_trends_ranked_measure_data <- read_xlsx("USA health trends ranked measure data.xlsx", 
+                                                   header     = TRUE,
+                                                   na.strings = "***")
 
 # Sidebar with a slider input
 fluidPage(
@@ -18,14 +22,18 @@ fluidPage(
                              choices = unique(Sample_R_Data$State)
                              ),
                             
-                             
-                             selectInput(inputId = 'value' ,
-                             label = 'Select Value' ,
-                            choices = colnames(Sample_R_Data)[5:ncol(Sample_R_Data)])
+                      
+                             selectInput(inputId = 'value1' ,
+                             label = 'Select First Value' ,
+                            choices = colnames(USA_health_trends_ranked_measure_data)[4:ncol(USA_health_trends_ranked_measure_data)])
                
                ),
           
-
+                              selectInput(inputId = 'value2' ,
+                             label = 'Select Second Value' ,
+                             choices = colnames(USA_health_trends_ranked_measure_data)[4:ncol(USA_health_trends_ranked_measure_data)]
+                             
+                              ),
 
 #Panel plot
 mainPanel(
