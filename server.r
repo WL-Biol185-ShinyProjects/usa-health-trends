0bed6ece170b5863baaf2b90fe030d47d2433965
Lauren Hoffman <hoffmanl18@mail.wlu.edu>
2017-11-07 12:17:59 -0500

    Fixing bugs in read.csv code

--- server.r
+++ server.r
@@ -5,11 +5,11 @@ library(ggplot2)
 library(readr)
 library(leaflet)
 
-USA_health <- read.csv("USA health.csv", 
-                       col.names     = TRUE, 
-                       stringsAsFactors = FALSE, 
-                       header = TRUE, sep = ";",
-                       na.strings = ("***"), as.numeric([4:ncol(USA_health)]))
+USA_health <- read_csv("~/usa-health-trends/USA health.csv", na = "***")   
+USA_health[20:21] <- NULL
+USA_health[24] <- NULL
+USA_health[27] <- NULL
+USA_health[59] <- NULL
 
 #define server logic to draw historgram 
 function(input, output) {
--- ui.R
+++ ui.R
@@ -4,13 +4,12 @@ library(readr)
 library(leaflet)
 
 
-USA_health <- read.csv("USA health.csv", 
-                                  col.names     = TRUE, 
-                                  stringsAsFactors = FALSE, 
-                                  header = TRUE, sep = ";",
-                                  na.strings = "***", as.numeric(4:ncol(USA_health)))
- 
-  
+USA_health <- read_csv("~/usa-health-trends/USA health.csv", na = "***")
+USA_health[20:21] <- NULL
+USA_health[24] <- NULL
+USA_health[27] <- NULL
+USA_health[59] <- NULL
+
 # Sidebar with a slider input
 fluidPage(
   
