2d956088e56252e1a6ba216019faede988395a3a
Lauren Hoffman <hoffmanl18@mail.wlu.edu>
2017-11-07 12:45:39 -0500

    HALLELUJAH WE FIXED THE PROBLEM PART 2

--- server.r
+++ server.r
@@ -6,6 +6,9 @@ library(readr)
 library(leaflet)
 
 USA_health <- read_csv("~/usa-health-trends/USA health 2.csv", na = "***")
+USA_health$State <- factor(USA_health$State)
+USA_health$County <- factor(USA_health$County)
+
 
 
 #define server logic to draw historgram 
