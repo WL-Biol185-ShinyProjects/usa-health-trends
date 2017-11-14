a97f34b28ec48e0497243e2cacade52828239291
Hannah Palmatary <palmataryh18@mail.wlu.edu>
2017-11-14 12:47:35 -0500

    Map appears in app!!

--- server.r
+++ server.r
@@ -19,24 +19,26 @@ function(input, output) {
   
   output$health_plot <- renderPlot ({
   
-
-   USA_health %>%
-      filter(State == input$State) %>%
-      ggplot(aes_string("County", input$value1)) + geom_bar(stat = "identity", fill(input$value2)) + 
-      theme(axis.text.x = element_text(angle = 60, hjust = 1))
-
-
-  })} 
-    
-function(input, output) {
   
-map = leaflet() %>% addTiles() setView(-93.65, 42.0285, zoom = 17)
+       USA_health %>%
+          filter(State == input$State) %>%
+          ggplot(aes_string("County", input$value1)) + geom_bar(stat = "identity", fill(input$value2)) + 
+          theme(axis.text.x = element_text(angle = 60, hjust = 1))
 
-output$state_map <- renderLeaflet(map)
+
+  }) 
     
+
+  
+
+
+  output$state_map <- renderLeaflet({
+    
+    leaflet() %>% addTiles() %>% setView(-93.65, 42.0285, zoom = 17)
+    
+  })
    
-    
-  }    
+}
 
 
 
