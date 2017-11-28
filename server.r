e5d72f6fd754f9999de4c236437eac2eab01f679
Lauren Hoffman <hoffmanl18@mail.wlu.edu>
2017-11-28 12:28:59 -0500

    HEAT MAP WORKS!!!!!!!!!!

--- server.r
+++ server.r
@@ -4,6 +4,9 @@ library(tidyverse)
 library(ggplot2)
 library(readr)
 library(leaflet)
+library(RColorBrewer)
+library(dplyr)
+
 
 
 
@@ -13,55 +16,57 @@ USA_health$County <- factor(USA_health$County)
 
 
 
-
 #define server logic to draw historgram 
 function(input, output) {
   
   output$health_plot <- renderPlot ({
-  
-  
-       USA_health %>%
-          filter(State == input$State) %>%
-          ggplot(aes_string("County", input$value1)) + geom_bar(stat = "identity", fill(input$value2)) + 
-          theme(axis.text.x = element_text(angle = 60, hjust = 1))
-
-
-  }) 
     
-
+    
+    USA_health %>%
+      filter(State == input$State) %>%
+      ggplot(aes_string("County", input$value1)) + geom_bar(stat = "identity", fill(input$value2)) + 
+      theme(axis.text.x = element_text(angle = 60, hjust = 1))
+    
+    
+  }) 
   
-
   statesGEO  <- rgdal::readOGR("states.geo.json", "OGRGeoJSON")
   stateCodes <- read_csv("states.csv")
   stateHealth <- read_csv("overall state health.csv")
-
   
-
   statesGEO@data <- 
     statesGEO@data %>%
-    left_join(stateCodes, by = c("NAME" = "State")) %>%
-    left_join(stateHealth, by = c("Abbreviation" = "State Name"))
-  
+    left_join(stateHealth, by = c("NAME" = "State Name")) #%>%
+    #left_join(stateHealth, by = c("State" = "State Name"))
   
   output$state_map <- renderLeaflet({
-  
+    
+    #bins <- c(1, 10, 20, 30, 40, 50)
+    pal <- colorBin("YlOrRd", domain = c(1,50), bins = 5, pretty = TRUE, na.color = "#809000",
+                    alpha = FALSE, reverse = FALSE)
+    
+    
     m <- leaflet(statesGEO) %>%
       setView(-96, 37.8, 4) %>%
       addTiles()
     
-    
-    m %>% 
-      addPolygons()
-    
-    
-    
+    m %>% addPolygons(
+      fillColor = ~pal(Rank),
+      weight = 2,
+      opacity = 1,
+      color = "white",
+      dashArray = "3",
+      fillOpacity = 0.7,
+      highlight = highlightOptions(
+        weight = 5,
+        color = "#667",
+        dashArray = "",
+        fillOpacity = 0.7,
+        bringToFront = TRUE))
     
   })
   
-  
-  }
-   
-
+}
 
 
 
